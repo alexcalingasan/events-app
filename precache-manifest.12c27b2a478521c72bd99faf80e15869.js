f3e511605ab5fb129647c926983ecb0204262c28
Alexis Calingasan <alexiscalingasan@gmail.com>
2019-07-18 01:51:34 +0800

    Updates

---
+++ precache-manifest.12c27b2a478521c72bd99faf80e15869.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27b7f43056bed7026d049e92df978d31",
+    "url": "/events-app/index.html"
+  },
+  {
+    "revision": "d85b3fe35f047610e390",
+    "url": "/events-app/static/css/2.cc034990.chunk.css"
+  },
+  {
+    "revision": "e1622e8127f91d226b1c",
+    "url": "/events-app/static/css/main.d530f37f.chunk.css"
+  },
+  {
+    "revision": "d85b3fe35f047610e390",
+    "url": "/events-app/static/js/2.bb6d6ff6.chunk.js"
+  },
+  {
+    "revision": "e1622e8127f91d226b1c",
+    "url": "/events-app/static/js/main.e87f5fed.chunk.js"
+  },
+  {
+    "revision": "1932a46a3ff11e2310cc",
+    "url": "/events-app/static/js/runtime~main.ebfbe037.js"
+  }
+]);
