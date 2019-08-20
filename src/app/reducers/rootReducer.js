beb48b1f0ec76886e27b6853c6d1789ecfcd2861
Alexis Calingasan <alexiscalingasan@gmail.com>
2019-08-21 03:57:29 +0800

    add react-redux-toastr

--- src/app/reducers/rootReducer.js
+++ src/app/reducers/rootReducer.js
@@ -2,6 +2,7 @@ import { combineReducers } from "redux";
 import testReducer from "../../features/TestArea/testReducer";
 import eventReducer from "../../features/event/eventReducer";
 import {reducer as FormReducer} from "redux-form";
+import {reducer as ToastrReducer} from "react-redux-toastr";
 import modalReducer from "../../features/modal/modalReducer";
 import authReducer from "../../features/auth/authReducer";
 import asyncReducer from "../../features/async/asyncReducer";
@@ -12,7 +13,8 @@ const rootReducer = combineReducers({
     form: FormReducer,
     modals: modalReducer,
     auth: authReducer,
-    async: asyncReducer
+    async: asyncReducer,
+    toastr: ToastrReducer
 });
 
 export default rootReducer;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "react-redux-toastr/lib/css/react-redux-toastr.min.css";
 import "./index.css";
 import App from "./app/layout/App";
 import * as serviceWorker from "./serviceWorker";
@@ -8,6 +9,7 @@ import { Provider } from "react-redux";
 import { configureStore } from "./app/store/configureStore";
 import ScrollToTop from "./app/common/util/scrollToTop";
 import { loadEvents } from "./features/event/eventActions";
+import ReduxToastr from "react-redux-toastr";
 
 const store = configureStore();
 store.dispatch(loadEvents());
@@ -16,6 +18,11 @@ ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
     <ScrollToTop>
+      <ReduxToastr
+      position='bottom-right'
+      transitionIn='fadeIn'
+      transitionOut='fadeOut'
+      />
       <App />
     </ScrollToTop>
     </BrowserRouter>
